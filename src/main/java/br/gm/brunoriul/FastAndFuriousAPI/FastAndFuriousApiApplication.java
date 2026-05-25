6cdd92ed7c6b7bfaa69d14a3c2276a1b4ca5a82e
Bruno Riul <brunoc.riul@gmail.com>
2026-05-25 14:04:17 -0300

    Início do projeto - Adicionado apenas configurações nas propriedades (Other Sources > src/main/resources > <default package> - application.properties modificado; application-test.properties criado e modificado - Apontada a direção de classe principal (br.gm.brunoriul.FastAndFuriousAPI.FastAndFuriousApiApplication)

---
+++ src/main/java/br/gm/brunoriul/FastAndFuriousAPI/FastAndFuriousApiApplication.java
@@ -0,0 +1,13 @@
+package br.gm.brunoriul.FastAndFuriousAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastAndFuriousApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastAndFuriousApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gm/brunoriul/FastAndFuriousAPI/FastAndFuriousApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gm.brunoriul.FastAndFuriousAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAndFuriousApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
